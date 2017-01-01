fe9860f15c317aec0da124206654b241b8d9e287
Lakshitha Herath <lakshitha@Lakshithas-MacBook-Pro.local>
2017-01-01 20:13:33 +0530

    initial commit

---
+++ app/components/user.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'user',
+    templateUrl: 'user.component.html',
+})
+export class UserComponent {
+
+    name: string;
+    email: string;
+    address: address;
+    hobbies: string[];
+    showHobbies: boolean;
+
+    constructor() {
+        this.name = "Scooby Doo";
+        this.email = "lak@gmail.com";
+        this.address = {
+            street: "Hill Road",
+            city: "Matale",
+            state: "Central"
+        };
+        this.hobbies = ["Movies", "Reading", "Sports"];
+        this.showHobbies = false;
+    }
+
+
+    togglesHobbies() {
+        if (this.showHobbies) {
+            this.showHobbies = false;
+        }
+        else {
+            this.showHobbies = true;
+        }
+    }
+
+    addHobby() {
+        this.hobbies.push("Foo");
+    }
+
+    deleteHobby(index: number) {
+        this.hobbies.splice(index, 1);
+    }
+
+}
+
+
+interface address {
+    street: string;
+    city: string;
+    state: string;
+}
